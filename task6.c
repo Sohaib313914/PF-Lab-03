581eace5991b0af7cc2febcc615bf2b5d4470260
Sohaib313914 <smsohaib26@gmail.com>
2026-02-08 13:39:27 +0500

    Added escape sequence program

--- task6.c
+++ task6.c
@@ -2,7 +2,7 @@
 
 int main() {
     printf("Name:\tSyed Muhammad Sohaib\n");
-    printf("Roll No:\tYourRollNumber\n");
-    printf("Section:\tYourSection\n");
+    printf("Roll No:\t25k-6532\n");
+    printf("Section:\tCE-2A\n");
     return 0;
 }
